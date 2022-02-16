40477145e34cd37e1430dbe291a6de4676518648
Mariettegg <100428980@alumnos.uc3m.es>
2022-02-15 19:51:36 -0500

    Changes done by advide of teacher

--- main.py
+++ main.py
@@ -19,7 +19,35 @@ def getPos(word ,letter):
             index += 1
 
 def getSimilar():
+    alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    result = []
+
+    #obtaining the word from console
     word = input("input >> ")
+
+    #Calling the function to obtain the pattern
+    pattern = getPattern(word)
+
+    #Displaying the pattern as letters, not as a list
+    patternword = ""
+    for x in pattern:
+        patternword += alphabet[x]
+    print("The pattern of the inserted word is: ", patternword.lower())
+
+    #Asking user if wants the posible matches of the pattern with a database
+    matches = input("Would like to find pattern similarities from a database? (1) Yes, (2) No >> ")
+    try:
+        if int(matches) not in [1,2]:
+            print("That is not an option")
+            return 0
+        elif int(matches) == 2:
+            return 0
+
+    except:
+        print("That is not an option")
+        return 0
+
+    #Opening database of words
     file = input("word database >> ")
     try:
         f = open(file, "r")
@@ -30,11 +58,7 @@ def getSimilar():
 
     words_alpha = f.readlines()
 
-
-    result = []
-
-    pattern = getPattern(word)
-
+    #Finding the matches in the between the database and the input word.
     for sample in words_alpha:
         sample = sample[:-1]
         if len(sample) == len(word):
